9b2ef95ca6ef5c04d8f0ec01e1232df9ccf3013d
Travis Roman <ffx1033@gmail.com>
2015-11-13 19:43:54 -0500

    Initial Commit

---
+++ Alarm Pad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alarm Pad
+//
+//  Created by Travis Roman on 11/13/15.
+//  Copyright © 2015 Travis Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
